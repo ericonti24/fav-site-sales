e2135b4e1823635b539f36c049d200c94169d161
Eric <ericontiveros123@gmail.com>
2024-03-28 09:36:28 -0700

    add error messages in modal

--- app/sites/page.js
+++ app/sites/page.js
@@ -19,6 +19,7 @@ const Sites = () => {
   const [showModal, setShowModal] = useState(false);
   const [showDropdowns, setShowDropdowns] = useState(Array(sites.length).fill(false));
   const [folderId, setFolderId] = useState(uuidv4());
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
@@ -62,6 +63,12 @@ const Sites = () => {
     try {
       if (!currentUser) {
         console.error('Current user is undefined');
+        return;
+      }
+
+      if (!siteName || !url || !imageFile) {
+        console.error('All fields are required');
+        // alert('All fields are required');
         return;
       }
   
@@ -153,85 +160,6 @@ const Sites = () => {
           <p className='mb-8'>Then click on your uploaded url to view and save on your favorite site!</p>
           <button onClick={() => setShowModal(true)} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mb-8">Add a new site</button>
         </div>
-        {/* <div className="w-full max-w-md">
-      {sites.map((site, index) => (
-        <div key={index} className="bg-white border border-gray-200 rounded-lg shadow mb-8">
-          <div className="flex justify-end px-4 pt-2">
-            <div className="relative">
-              <button
-                id={`dropdownButton${index}`}
-                className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
-                type="button"
-                onClick={() => toggleDropdown(index)}
-              >
-                <span className="sr-only">Open dropdown</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 3"
-                >
-                  <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                </svg>
-              </button>
-              <div
-                id={`dropdown${index}`}
-                className={`absolute right-0 top-full ${
-                  showDropdowns[index] ? "block" : "hidden"
-                } z-10 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
-              >
-                <ul className="py-2" aria-labelledby={`dropdownButton${index}`}>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Edit
-                    </a>
-                  </li>
-                  <li>
-                    <button
-                      onClick={() => handleRemoveSite(site.id)}
-                      className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Delete
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center space-x-6 px-2 py-1 pb-6">
-            {site.imageUrl && (
-              <img
-                className="w-27 h-40 rounded-full shadow-lg"
-                src={site.imageUrl}
-                alt="Site image"
-              />
-            )}
-            <div className="flex flex-col flex-grow">
-              <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-black">
-                {site.siteName}
-              </h5>
-              <span className="text-sm text-gray-500 dark:text-gray-400">
-                {site.role}
-              </span>
-              <div className="flex mt-4 md:mt-6">
-                <a
-                  href={site.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Go to Sales
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div> */}
       <div className="w-full max-w-md">
         {sites.map((site, index) => (
           <div key={index} className="bg-white border border-gray-200 rounded-lg shadow mb-8">
@@ -322,6 +250,7 @@ const Sites = () => {
             <div className="mb-4">
               <label htmlFor="siteName" className="block font-semibold mb-2 text-black">Site Name:</label>
               <input type="text" id="siteName" value={siteName} onChange={handleSiteNameChange} className="w-full border rounded px-3 py-2 text-black" />
+              {showError && !siteName && <p className="text-red-500 mt-1">Site Name is required</p>}
             </div>
             <div className="mb-4">
               <label htmlFor="image" className="block font-semibold mb-2 text-black">Add Photo:</label>
@@ -334,13 +263,15 @@ const Sites = () => {
                   <button onClick={handleRemoveImage} className="bg-red-600 text-black px-1 py-1 rounded hover:bg-red-700">Remove</button>
                 </div>
               )}
+              {showError && !imageFile && <p className="text-red-500 mt-1">Image is required</p>}
             </div>
             <div className="mb-4">
               <label htmlFor="url" className="block font-semibold mb-2 text-black">URL Link:</label>
               <input type="text" id="url" value={url} onChange={handleUrlChange} className="w-full border rounded px-3 py-2 text-black" />
+              {showError && !url && <p className="text-red-500 mt-1">URL Link is required</p>}
             </div>
             <div className="flex justify-end">
-              <button onClick={handleAddSite} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Add Site</button>
+              <button onClick={() => { setShowError(true); handleAddSite(); }} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Add Site</button>
               <button onClick={() => setShowModal(false)} className="bg-gray-300 text-gray-700 px-4 py-2 rounded ml-4 hover:bg-gray-400">Cancel</button>
             </div>
           </div>
